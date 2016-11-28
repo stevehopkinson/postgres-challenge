f29b6d6667b5e169d3d86331c3f2f7514e5827f5
Nick Field <heyimnick@live.com>
2016-11-28 22:04:57 +0000

    remove partials directory

--- src/configure_handlebars.js
+++ src/configure_handlebars.js
@@ -7,7 +7,6 @@ module.exports = (server) => {
     path: './views',
     layout: 'default',
     layoutPath: './views/layout',
-    partialsPath: './views/partials',
     helpersPath: './views/helpers',
   });
 
