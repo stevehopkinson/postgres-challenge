8830889126d3277d0f5f19d7169c12bfff22289c
Nick Field <heyimnick@live.com>
2016-11-28 21:25:32 +0000

    update css, add environment variables for live database, logic to handle live database url, refactored user_query

--- src/db_connection.js
+++ src/db_connection.js
@@ -1,10 +1,11 @@
-const { Pool } = require('pg');
+const Pool = require('pg').Pool;
 const url = require('url');
 
-if (!process.env.DB_URL) throw new Error('Environment variable DB_URL must be set');
+const DB_URL = process.env.DB_LIVEURL ? process.env.DB_LIVEURL : process.env.DB_URL;
+if (!DB_URL) throw new Error('Environment variable DB_URL must be set');
 
-const params = url.parse(process.env.DB_URL);
-// const [username, password] = params.auth.split(':');
+const params = url.parse(DB_URL);
+const [username, password] = params.auth.split(':');
 
 const options = {
   host: params.hostname,
@@ -13,8 +14,8 @@ const options = {
   max: process.env.DB_MAX_CONNECTIONS || 2,
 };
 
-// if (username) options.user = username;
-// if (password) options.password = password;
+if (username) options.user = username;
+if (password) options.password = password;
 options.ssl = (options.host !== 'localhost');
 
 module.exports = new Pool(options);
--- src/init_database.js
+++ src/init_database.js
@@ -1,16 +1,14 @@
-module.exports = (function () {
-  const env = require('env2');
-  const fs = require('fs');
+const env = require('env2');
+const fs = require('fs');
 
-  env('./config.env');
+env('./config.env');
 
-  const dbConn = require('./db_connection');
+const dbConn = require('./db_connection');
 
-  const sql = fs.readFileSync(`${__dirname}/init_database.sql`).toString();
+const sql = fs.readFileSync(`${__dirname}/init_database.sql`).toString();
 
-  dbConn
-  .query(sql, (error, result) =>
-    (error
-      ? console.log('Error', error)
-      : console.log('Result', result)));
-}());
+const generate = function () {
+  dbConn.query(sql, (error) => { if (error) throw error; });
+};
+
+module.exports = generate();
